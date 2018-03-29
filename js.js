bedf79e67efa88c2ef2877b164abf76aac517bd5
St3ve89 <istvan.acs89@gmail.com>
2018-03-29 13:10:13 +0100

    My very first Js challange

---
+++ js.js
@@ -0,0 +1,10 @@
+
+alert("Let's Begin The Challenge!");
+
+document.write("Congrats You Fixed This Line Of Code!";
+
+
+Console.lo("Good Job Your Almost There!");
+
+Console.log("If this appears in the console then you have completed the challenge, Well Done!")
+
